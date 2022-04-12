18dc7c0ea85cd296f846a4ad163edc941a415d51
Tucker Deaton <tucker11@ksu.edu>
2022-04-11 22:18:53 -0500

    Added adn Admin to the suer. Server will ask for the admin to login before starting the server. Added a form of panels for when an admin logs in can keep track of 1) users registers 2) users online 3) chat rooms opened and users in those chat rooms.

---
+++ Websocket-Server/Websocket-Server/AdminPanel.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Websocket_Server
+{
+    partial class AdminPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxChatRoomsListBox = new System.Windows.Forms.ListBox();
+            this.uxConnectedUsersListBox = new System.Windows.Forms.ListBox();
+            this.uxRegisterUsersListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // uxChatRoomsListBox
+            // 
+            this.uxChatRoomsListBox.FormattingEnabled = true;
+            this.uxChatRoomsListBox.Location = new System.Drawing.Point(302, 12);
+            this.uxChatRoomsListBox.Name = "uxChatRoomsListBox";
+            this.uxChatRoomsListBox.Size = new System.Drawing.Size(120, 95);
+            this.uxChatRoomsListBox.TabIndex = 5;
+            // 
+            // uxConnectedUsersListBox
+            // 
+            this.uxConnectedUsersListBox.FormattingEnabled = true;
+            this.uxConnectedUsersListBox.Location = new System.Drawing.Point(155, 12);
+            this.uxConnectedUsersListBox.Name = "uxConnectedUsersListBox";
+            this.uxConnectedUsersListBox.Size = new System.Drawing.Size(120, 95);
+            this.uxConnectedUsersListBox.TabIndex = 4;
+            // 
+            // uxRegisterUsersListBox
+            // 
+            this.uxRegisterUsersListBox.FormattingEnabled = true;
+            this.uxRegisterUsersListBox.Location = new System.Drawing.Point(4, 12);
+            this.uxRegisterUsersListBox.Name = "uxRegisterUsersListBox";
+            this.uxRegisterUsersListBox.Size = new System.Drawing.Size(120, 95);
+            this.uxRegisterUsersListBox.TabIndex = 3;
+            // 
+            // AdminPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 152);
+            this.Controls.Add(this.uxChatRoomsListBox);
+            this.Controls.Add(this.uxConnectedUsersListBox);
+            this.Controls.Add(this.uxRegisterUsersListBox);
+            this.Name = "AdminPanel";
+            this.Text = "AdminPanel";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox uxChatRoomsListBox;
+        private System.Windows.Forms.ListBox uxConnectedUsersListBox;
+        private System.Windows.Forms.ListBox uxRegisterUsersListBox;
+    }
+}
--- Websocket-Server/Websocket-Server/Program.cs
+++ Websocket-Server/Websocket-Server/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using WebSocketSharp;
 using WebSocketSharp.Server;
 
@@ -12,6 +13,10 @@ namespace Websocket_Server
     {
         static void Main(string[] args)
         {
+            bool serverStatus = false;
+            Dictionary<String, String> adminCredentials = new Dictionary<string, string>();
+            adminCredentials.Add("admin", "password");
+
             // Start a websocket server at port 8001
             var wss = new WebSocketServer(8001);
 
@@ -21,13 +26,38 @@ namespace Websocket_Server
             // Add the Chat websocket service
             wss.AddWebSocketService<Chat>("/chat");
 
-            
+            while (serverStatus == false)
+            {
+                Console.WriteLine("Enter Admin Username: ");
+                string tempAdminUser = Console.ReadLine();
 
-            // Start the server
-            wss.Start();
+                foreach (string s in adminCredentials.Keys)
+                {
+                    if (s.Equals(tempAdminUser))
+                    {
+                        Console.WriteLine("Enter Admin Password: ");
+                        string tempAdminPass = Console.ReadLine();
+                        foreach (string t in adminCredentials.Values)
+                        {
+                            if (t.Equals(tempAdminPass))
+                            {
+                                serverStatus = true;
 
+                                // Start the server
+                                wss.Start();
+                            }
+                        }
+                    }
+                }
+            }
+
+            AdminPanel form = new AdminPanel();
+            form.Show();
+
+            Console.WriteLine("\n Server Is Online");
             Console.WriteLine("Press Enter to exit.");
             Console.ReadLine();
+            
 
             // Stop the server
             wss.Stop();
