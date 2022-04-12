b2a8cf0b67d469563f10f436dfa65e1268fb9a89
Ethan Khoury <khouryethan@ksu.edu>
2022-04-11 21:54:28 -0500

    Added LoginForm (Needs Work)

---
+++ Websocket-Client-Chat/Websocket-Client-Chat/CredentialsForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Websocket_Client_Chat
+{
+    public partial class uxCredentialsForm : Form
+    {
+        public uxCredentialsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CredentialsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string GetPassword()
+        {
+            return uxPasswordInput.Text;
+        }
+
+        private string GetUsername()
+        {
+            return uxUsernameInput.Text;
+        }
+
+        private void uxLogin_Click(object sender, EventArgs e)
+        {
+            foreach (User u in allUsers)
+            {
+                if (u.Username == GetUsername())
+                {
+                    Login(u);
+                    return;
+                }
+            }
+            
+            User u = new User();
+            u.Username = GetUsername();
+            u.Password = GetPassword();
+            u.Status = "Online";
+            allUsers.Add(u);
+            Login(u);
+
+        }
+
+        private void Login(User u)
+        {
+
+        }
+    }
+}
--- Websocket-Client-Chat/Websocket-Client-Chat/Program.cs
+++ Websocket-Client-Chat/Websocket-Client-Chat/Program.cs
@@ -20,68 +20,76 @@ namespace Websocket_Client_Chat
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            string name = GetName();
+            public static List<User> allUsers = new List<User>();
 
-            ChatController c = new ChatController(name);
-            ChatForm f = new ChatForm(name, c.MessageEntered);
-            c.MessageReceived += f.MessageReceived;
+            public uxCredentialsForm loginForm = new uxCredentialsForm();
+            loginForm.Show();
 
-            Application.Run(f);
+            
+
+            //string name = GetName();
+
+
+            //ChatController c = new ChatController(name);
+            //ChatForm f = new ChatForm(name, c.MessageEntered);
+            //c.MessageReceived += f.MessageReceived;
+
+            Application.Run(loginForm);
         }
 
         // Ask for a name (that is a non-empty string)
-        private static string GetName()
-        {
-            string name = "";
-            do
-            {
-                while (InputBox("Name", "Enter user name:", ref name) != DialogResult.OK) ;
-            }
-            while (name == "");
-            return name;
-        }
+        //private static string GetName()
+        //{
+        //    string name = "";
+        //    do
+        //    {
+        //        while (InputBox("Name", "Enter user name:", ref name) != DialogResult.OK) ;
+        //    }
+        //    while (name == "");
+        //    return name;
+        //}
 
-        // From http://www.csharp-examples.net/inputbox/
-        public static DialogResult InputBox(string title, string promptText, ref string value)
-        {
-            Form form = new Form();
-            Label label = new Label();
-            TextBox textBox = new TextBox();
-            Button buttonOk = new Button();
-            Button buttonCancel = new Button();
+        //// From http://www.csharp-examples.net/inputbox/
+        //public static DialogResult InputBox(string title, string promptText, ref string value)
+        //{
+        //    Form form = new Form();
+        //    Label label = new Label();
+        //    TextBox textBox = new TextBox();
+        //    Button buttonOk = new Button();
+        //    Button buttonCancel = new Button();
 
-            form.Text = title;
-            label.Text = promptText;
-            textBox.Text = value;
+        //    form.Text = title;
+        //    label.Text = promptText;
+        //    textBox.Text = value;
 
-            buttonOk.Text = "OK";
-            buttonCancel.Text = "Cancel";
-            buttonOk.DialogResult = DialogResult.OK;
-            buttonCancel.DialogResult = DialogResult.Cancel;
+        //    buttonOk.Text = "OK";
+        //    buttonCancel.Text = "Cancel";
+        //    buttonOk.DialogResult = DialogResult.OK;
+        //    buttonCancel.DialogResult = DialogResult.Cancel;
 
-            label.SetBounds(9, 20, 372, 13);
-            textBox.SetBounds(12, 36, 372, 20);
-            buttonOk.SetBounds(228, 72, 75, 23);
-            buttonCancel.SetBounds(309, 72, 75, 23);
+        //    label.SetBounds(9, 20, 372, 13);
+        //    textBox.SetBounds(12, 36, 372, 20);
+        //    buttonOk.SetBounds(228, 72, 75, 23);
+        //    buttonCancel.SetBounds(309, 72, 75, 23);
 
-            label.AutoSize = true;
-            textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
-            buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        //    label.AutoSize = true;
+        //    textBox.Anchor = textBox.Anchor | AnchorStyles.Right;
+        //    buttonOk.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        //    buttonCancel.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
 
-            form.ClientSize = new Size(396, 107);
-            form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
-            form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
-            form.FormBorderStyle = FormBorderStyle.FixedDialog;
-            form.StartPosition = FormStartPosition.CenterScreen;
-            form.MinimizeBox = false;
-            form.MaximizeBox = false;
-            form.AcceptButton = buttonOk;
-            form.CancelButton = buttonCancel;
+        //    form.ClientSize = new Size(396, 107);
+        //    form.Controls.AddRange(new Control[] { label, textBox, buttonOk, buttonCancel });
+        //    form.ClientSize = new Size(Math.Max(300, label.Right + 10), form.ClientSize.Height);
+        //    form.FormBorderStyle = FormBorderStyle.FixedDialog;
+        //    form.StartPosition = FormStartPosition.CenterScreen;
+        //    form.MinimizeBox = false;
+        //    form.MaximizeBox = false;
+        //    form.AcceptButton = buttonOk;
+        //    form.CancelButton = buttonCancel;
 
-            DialogResult dialogResult = form.ShowDialog();
-            value = textBox.Text;
-            return dialogResult;
-        }
+        //    DialogResult dialogResult = form.ShowDialog();
+        //    value = textBox.Text;
+        //    return dialogResult;
+        //}
     }
 }
